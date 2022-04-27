47887166923aeb3021eeae66cfe8c765ac8161af
FoxyHart <88792549+FoxyHart@users.noreply.github.com>
2022-04-27 12:00:52 -0400

    4.3 adding more

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -8,5 +8,6 @@ describe('<EventList /> component', () => {
     test('render correct number of events', () => {
       const EventListWrapper = shallow(<EventList events={mockData} />);
       expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
-    })
+    });
+    
 });
